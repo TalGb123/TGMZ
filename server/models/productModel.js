497f5f368f5fbfb1b67bcc2f6b42f7cd03b4d4b3
Tal Gubenko <leonardotal2005@gmail.com>
2026-04-30 10:11:39 +0300

    for now updated only the product models to allow maxim work on his current part

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -5,74 +5,88 @@ const productOptions = { discriminatorKey: 'category', collection: 'products' };
 
 const productSchema = new Schema({
   name: { type: String, required: true, index: true },
-  price: { type: Number, default: null },
+  price: { type: Number, default: 999999 },
+  brand: { type: String, required: true },
   image: { type: String },
   inStock: { type: Boolean, default: true },
-  color: { type: String, default: null },
 }, productOptions);
 
 const Product = mongoose.models.Product || mongoose.model('Product', productSchema);
 
 const CPU = mongoose.models.CPU || Product.discriminator('CPU', new Schema({
+  socket: { type: String, required: true },
   core_count: { type: Number, required: true },
   core_clock: { type: Number, required: true },
   boost_clock: { type: Number, default: null },
-  microarchitecture: { type: String },
-  tdp: { type: Number },
-  graphics: { type: String, default: null } 
+  tdp: { type: Number, required: true },
+  has_apu: { type: Boolean, default: false },
+  supported_memory: { type: [String], required: true }
 }));
 
 const CPUCooler = mongoose.models.CPUCooler || Product.discriminator('CPUCooler', new Schema({
-  rpm: { type: Schema.Types.Mixed }, 
-  noise_level: { type: Schema.Types.Mixed }, 
-  size: { type: Number, default: null }
+  type: { type: String, required: true },
+  supported_sockets: { type: [String], required: true },
+  height: { type: Number }, 
+  radiator_size: { type: Number, default: 0 }, 
+  noise_level: { type: Number },
+  max_tdp_cooling: { type: Number, required: true },
+  color: { type: String }
 }));
 
 const Motherboard = mongoose.models.Motherboard || Product.discriminator('Motherboard', new Schema({
   socket: { type: String, required: true },
   form_factor: { type: String, required: true },
-  max_memory: { type: Number },
-  memory_slots: { type: Number }
+  memory_gen: { type: String, required: true }, 
+  memory_slots: { type: Number, required: true },
+  has_wifi_bluetooth: { type: Boolean, default: false },
+  m2_slots: { type: Number },
+  connections: { type: [String] },
+  color: { type: String }
 }));
 
 const Memory = mongoose.models.Memory || Product.discriminator('Memory', new Schema({
-  speed: { type: [Number], required: true },
+  // e.g. ["DDR5", 5200, 40] -> gen, speed, CL
+  speed: { type: [Schema.Types.Mixed], required: true }, 
+  // e.g. [2, 32] -> stick_amount, capacity_per_stick
   modules: { type: [Number], required: true },
-  price_per_gb: { type: Number },
-  first_word_latency: { type: Number },
-  cas_latency: { type: Number }
+  color: { type: String }
 }));
 
 const Storage = mongoose.models.Storage || Product.discriminator('Storage', new Schema({
   capacity: { type: Number, required: true },
-  price_per_gb: { type: Number },
-  drive_type: { type: Schema.Types.Mixed, alias: 'type' },
-  cache: { type: Number, default: null },
-  form_factor: { type: Schema.Types.Mixed },
-  interface: { type: String }
+  drive_type: { type: String, alias: 'type', required: true },
+  form_factor: { type: String, required: true }
 }));
 
 const VideoCard = mongoose.models.VideoCard || Product.discriminator('VideoCard', new Schema({
   chipset: { type: String, required: true },
-  memory: { type: Number },
+  memory: { type: Number, required: true },
   core_clock: { type: Number },
   boost_clock: { type: Number, default: null },
-  length: { type: Number }
+  tdp: { type: Number, required: true },
+  length: { type: Number, required: true },
+  slots_required: { type: Number, required: true },
+  recommended_psu_wattage: { type: Number, required: true },
+  color: { type: String }
 }));
 
 const Case = mongoose.models.Case || Product.discriminator('Case', new Schema({
-  type: { type: String },
-  psu: { type: String, default: null },
-  side_panel: { type: String },
-  external_volume: { type: Number, default: null },
-  internal_35_bays: { type: Number }
+  type: { type: String, required: true },
+  supported_mobo_form_factors: { type: [String], required: true },
+  max_gpu_length: { type: Number, required: true },
+  max_cpu_cooler_height: { type: Number, required: true },
+  psu_form_factor: { type: String, required: true },
+  supported_radiators: { type: [Number] },
+  sidepanel_material: { type: String },
+  color: { type: String }
 }));
 
 const PowerSupply = mongoose.models.PowerSupply || Product.discriminator('PowerSupply', new Schema({
-  type: { type: String },
+  type: { type: String, required: true },
   efficiency: { type: String, default: null },
   wattage: { type: Number, required: true },
-  modular: { type: Schema.Types.Mixed }
+  modular: { type: String, required: true },
+  color: { type: String }
 }));
 
 export {
